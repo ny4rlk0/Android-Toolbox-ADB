16e6af81b529043284455931758e2fd769ddce8d
ny4rlk0 <55476474+ny4rlk0@users.noreply.github.com>
2022-07-13 18:48:23 +0300

    Update AndroidToolbox.py

--- AndroidToolbox.py
+++ AndroidToolbox.py
@@ -4,7 +4,7 @@ import datetime;import time;from tkinter import *;from tkinter import messagebox
 nya=0
 rlko=0
 #Translation Variables
-lang="tr" # en, tr Change UI language with this variable
+lang="en" # en, tr Change UI language with this variable
 a1="";a2="";a3="";a4="";a5="";a6="";a7="";a8="";a9="";a10=""
 a11="";a12="";a13="";a14="";a15="";a16="";a17="";a18="";a19="";a20="";a21=""
 a22="";a23="";a24="";a25="";a26="";a27="";a28="";a29="";a30="";a31="";a32="";a33="";a34=""
